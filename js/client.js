19b94eb0963e1485a0c94ca86d3461fe5640f4ec
AdBouli <adrien.boulineau@laposte.net>
2016-01-29 00:05:12 +0100

    debut js

--- js/client.js
+++ js/client.js
@@ -1,13 +1,39 @@
 (function ($){
+
+	console.log('ok');
 	
 	var socket = io.connect("http://localhost:1337");
 	
-	$("#loginForm").submit(funtion(event){
+	$("#loginForm").submit(function(event){
 		event.preventDefault();
 		socket.emit("login", {
 			username : $("#username").val(),
 			mail     : $("#mail").val()
 		})
+	});
+
+	socket.on("logged", function(){
+		$("#loginForm").fadeOut();
+		$("#msg").focus();
 	})
+
+	$("#msgForm").submit(function(event){
+		event.preventDefault();
+		socket.emit("newMsg", {message : $("#msg").val() });
+		$("#msg").val("");
+		$("#msg").focus();
+	})
+
+	socket.on("newMsg", function(message){
+		$("#tchat").append("<p><strong>" + message.user.username °+ " :</strong> " + message + "</p><br>");
+	})
+
+	socket.on("newUser", function(user){
+		$("#users").append("<li id='" + user.id + "'>" + user.username + "</li>");
+	});
+
+	socket.on("discUser", function(user){
+		$("#" + user.id).remove();
+	});
 	
 })(jQuery);
--- server.js
+++ server.js
@@ -10,12 +10,41 @@ httpServer.listen(1337);
 
 var io = require("socket.io").listen(httpServer);
 
+var users = {};
+
 io.sockets.on("connection", function(socket){
 	
 	console.log("New user");
+	var me = false;
+
+	for(var k in users){
+		socket.emit("newUser", user[k]);
+	}
 	
 	socket.on("login", function(user){
-		console.log(user);
+		me = user;
+		me.id = user.mail.replace('@', '-').replace('.','-');
+		socket.emit("logged");
+		users[me.id] = me;
+		io.sockets.emit("newUser", me);
+	});
+
+	socket.on("newMsg", function(message){
+		message.user = me;
+		date = new Date();
+		message.h = date.getHours();
+		message.m = date.getMinutes();
+		io.sockets.emit("newMsg", message);
 	})
+
+	socket.on("disconnect", function(){
+		if(!me){
+			return false;
+		}
+		delete users[me.id];
+		io.sockets.emit('discUser', me);
+	});
+
+
 	
 });
